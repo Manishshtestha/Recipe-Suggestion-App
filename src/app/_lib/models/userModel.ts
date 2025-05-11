1df44b27522c9e966a49759de51142660eedcba4
Manish Shrestha <manishshtestha472@gmail.com>
2025-05-12 00:30:45 +0545

    Modified to add followers and following list

--- src/app/_lib/models/userModel.ts
+++ src/app/_lib/models/userModel.ts
@@ -4,15 +4,19 @@ export interface IUser extends Document {
 	name: string;
 	email: string;
 	password: string;
+    following: string[];
+    followers: string[];
 }
 
 const UserSchema: Schema<IUser> = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+    following:{type:[String],},
+    followers:{type:[String],},
 });
 
 const UserModel: Model<IUser> =
-    mongoose.models.Recipe || mongoose.model<IUser>("User", UserSchema);
+    mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
 
 export default UserModel;
