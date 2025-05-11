b8bcf31e1c48aab231ac661854cd11aac8a62e4b
Manish Shrestha <manishshtestha472@gmail.com>
2025-05-11 18:59:12 +0545

    Added User Model for Users to Register and Login

---
+++ src/app/_lib/models/userModel.ts
@@ -0,0 +1,18 @@
+import mongoose,{ Model, Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+	name: string;
+	email: string;
+	password: string;
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+});
+
+const UserModel: Model<IUser> =
+    mongoose.models.Recipe || mongoose.model<IUser>("User", UserSchema);
+
+export default UserModel;
